dff812cf5386d41aff03347d5904e4e28225fbac
aditya <aditya7238195@gmail.com>
2022-11-10 18:32:04 +0530

    changes added

--- client/src/components/Categories/CategoriesNav.js
+++ client/src/components/Categories/CategoriesNav.js
@@ -1,42 +1,57 @@
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Categories.css'
-import { BsHouseDoorFill, BsFillHouseFill, BsFillPuzzleFill } from 'react-icons/bs'
-import { AiFillCar } from 'react-icons/ai';
-import { GiFlowerPot, GiClothes } from 'react-icons/gi';
+import {
+  AiOutlineBook,
+  AiOutlineUser,
+  AiOutlineFundProjectionScreen,
+  AiFillFileText,
+  AiFillSetting,
+} from "react-icons/ai";
 import { TiSortAlphabetically } from 'react-icons/ti';
-import { MdPhoneAndroid } from 'react-icons/md'
 
 function CategoriesNav() {
     return (
-        <div className="container" id="categories">
-            <h1>Categories</h1>
-            <Link to="/categories/all">
-                <Button variant="dark" id="all"><TiSortAlphabetically />All</Button>{' '}
-            </Link>
-            <Link to="/categories/properties">
-                <Button variant="dark" id="properties"><BsHouseDoorFill />Properties</Button>{' '}
-            </Link>
-            <Link to="/categories/auto">
-                <Button variant="dark" id="auto"><AiFillCar />Auto</Button>{' '}
-            </Link>
-            <Link to="/categories/home">
-                <Button variant="dark" id="home"><BsFillHouseFill />Home</Button>{' '}
-            </Link>
-            <Link to="/categories/electronics">
-                <Button variant="dark" id="electronics"><MdPhoneAndroid />Electronics</Button>{' '}
-            </Link>
-            <Link to="/categories/clothes">
-                <Button variant="dark" id="clothes"><GiClothes />Clothes</Button>{' '}
-            </Link>
-            <Link to="/categories/toys">
-                <Button variant="dark" id="toys"><BsFillPuzzleFill />Toys</Button>{' '}
-            </Link>
-            <Link to="/categories/garden">
-                <Button variant="dark" id="garden"><GiFlowerPot />Garden</Button>{' '}
-            </Link>
-        </div>
-    )
+      <div className="container" id="categories">
+        <h1>Categories</h1>
+        <Link to="/categories/all">
+          <Button variant="dark" id="all">
+            <TiSortAlphabetically />
+            All
+          </Button>{" "}
+        </Link>
+        <Link to="/categories/properties">
+          <Button variant="dark" id="properties">
+            <AiOutlineBook />
+            Books
+          </Button>{" "}
+        </Link>
+        <Link to="/categories/auto">
+          <Button variant="dark" id="auto">
+            <AiOutlineFundProjectionScreen />
+            Projects
+          </Button>{" "}
+        </Link>
+        <Link to="/categories/home">
+          <Button variant="dark" id="home">
+            <AiOutlineUser />
+            Tutor
+          </Button>{" "}
+        </Link>
+        <Link to="/categories/electronics">
+          <Button variant="dark" id="electronics">
+            <AiFillFileText />
+            Notes
+          </Button>{" "}
+        </Link>
+        <Link to="/categories/clothes">
+          <Button variant="dark" id="clothes">
+            <AiFillSetting />
+            Hardware
+          </Button>{" "}
+        </Link>
+      </div>
+    );
 }
 
 export default CategoriesNav;
